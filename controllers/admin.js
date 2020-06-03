55ff7b16c4c63c400e7f7eab186fff17a0ef5cbf
Rohit Malik <bpsrohitmalik@gmail.com>
2020-06-03 21:28:15 +0530

    updated UPDATE_CONTACT_PAGE with having functionality of updating more than one phone and email field

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,20 +22,37 @@ exports.postAddContact = (req, res, next) => {
 exports.getContact = (req, res, next) => {
     const contactId = req.params.contactId;
     Contact.findById(contactId, contact => {
+        const is_tel_string =  (typeof contact.tel === "string") ? "string" : "array";
+        const is_email_string =  (typeof contact.email === "string") ? "string" : "array";
+        let firstTelEl = null;
+        let firstEmailEl = null;
+        if(is_tel_string === "array"){
+            firstTelEl = contact.tel.shift();
+        }
+        if(is_email_string === "array"){
+            firstEmailEl = contact.email.shift();
+        }
         res.render('admin/update-contact', {
             pageTitle: contact.name,
             path: '/add-contact',
-            contact: contact
-        })
+            contact: contact,
+            is_tel_string: is_tel_string,
+            is_email_string: is_email_string,
+            firstTelEl: firstTelEl,
+            firstEmailEl: firstEmailEl
+        });
     });
 };
 
 exports.postUpdatedContact = (req, res, next) => {
+    var tele = 'tel[]';
+    var mail = 'email[]';
     const contactId = req.body.id;
     const name = req.body.name;
     const dob = req.body.dob;
-    const tel = req.body.tel;
-    const email = req.body.email;
+    const tel = req.body[tele];
+    const email = req.body[mail];
+    console.log(req.body);
     Contact.updateContact(contactId, name, dob, tel, email);
     res.redirect('/');
 }
--- public/js/add-field.js
+++ public/js/add-field.js
@@ -7,7 +7,7 @@ function addTelField(divName){
         alert("You can only add " + counterTel + " contact numbers");
     }else{
        var newdiv = document.createElement('div');
-       newdiv.innerHTML = '<input class="input" type="tel" name="tel[]" id="tel" placeholder="Mobile Number" required>';
+       newdiv.innerHTML = '<input class="input" type="tel" name="tel[]" placeholder="Add another Mobile Number">';
        document.getElementById(divName).appendChild(newdiv);
        counterTel++;
     }
@@ -18,8 +18,14 @@ function addEmailField(divName){
         alert("You can only add " + counterEmail + " emails");
     }else{
         var newdiv = document.createElement('div');
-        newdiv.innerHTML = '<input class="input" type="tel" name="email[]" id="tel" placeholder="Mobile Number" required>';
+        newdiv.innerHTML = '<input class="input" type="tel" name="email[]" placeholder="Add another Email" required>';
         document.getElementById(divName).appendChild(newdiv);
         counterEmail++;
     }
+}
+
+function delete_extra(id){
+    document.getElementById(id).name = 'delete';
+    document.getElementById(id).style.display = 'none';
+    document.getElementById(id+'1').style.display = 'none';
 }
